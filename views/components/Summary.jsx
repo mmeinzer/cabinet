6bf54c4f0cde300f08e83528cced65dc9f3aded4
Matt Meinzer <meinzer.matthew@gmail.com>
2020-05-16 19:21:29 -0500

    Finish base styles

--- views/components/Summary.jsx
+++ views/components/Summary.jsx
@@ -1,26 +1,38 @@
 const React = require('react');
 const PropTypes = require('prop-types');
 
+const { formatDistanceToNowStrict } = require('date-fns');
+
 function Summary({ sample }) {
   return (
     <div className="flex justify-center mt-5">
-      <div className="flex flex-col text-right text-2xl">
+      <div className="flex flex-col text-center text-2xl leading-tight">
         {sample ? (
           <>
-            <p className="text-center text-sm font-light mt-2">Download</p>
+            <p className="text-2xl font-light text-gray-100">Internet Speed</p>
+            <p className="text-base text-gray-300">{`${formatDistanceToNowStrict(
+              sample.time
+            )} ago`}</p>
+            <p className="text-center text-sm font-light mt-3 text-gray-500">
+              Download
+            </p>
             <div>
               <span>{sample.download}</span>{' '}
-              <span className="font-thin text-gray-500">Mbps</span>
+              <span className="font-thin text-gray-500 text-xl">Mbps</span>
             </div>
-            <p className="text-center text-sm font-light mt-2">Upload</p>
+            <p className="text-center text-sm font-light mt-3 text-gray-500">
+              Upload
+            </p>
             <div>
               <span>{sample.upload}</span>{' '}
-              <span className="font-thin text-gray-500">Mbps</span>
+              <span className="font-thin text-gray-500 text-xl">Mbps</span>
             </div>
-            <p className="text-center text-xs font-light mt-2">Ping</p>
-            <p className="text-center text-lg">
+            <p className="text-center text-xs font-light mt-2 text-gray-500">
+              Ping
+            </p>
+            <p className="text-center text-lg text-gray-300 font-light">
               {sample.ping}
-              <span className="font-thin text-gray-500">ms</span>
+              <span className="font-thin text-gray-500 text-base">ms</span>
             </p>
           </>
         ) : (
