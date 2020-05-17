2053d70660919c90c34ac0302a93ae5c4bbae5b2
Matt Meinzer <meinzer.matthew@gmail.com>
2020-05-16 19:29:57 -0500

    Tweak styles

--- views/components/Summary.jsx
+++ views/components/Summary.jsx
@@ -9,8 +9,8 @@ function Summary({ sample }) {
       <div className="flex flex-col text-center text-2xl leading-tight">
         {sample ? (
           <>
-            <p className="text-2xl font-light text-gray-100">Internet Speed</p>
-            <p className="text-base text-gray-300">{`${formatDistanceToNowStrict(
+            <p className="text-2xl font-light text-gray-200">Internet Speed</p>
+            <p className="text-base text-gray-500 mb-3">{`${formatDistanceToNowStrict(
               sample.time
             )} ago`}</p>
             <p className="text-center text-sm font-light mt-3 text-gray-500">
